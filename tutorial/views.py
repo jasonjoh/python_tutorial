a6d95e4f3f2fc93ecf674900e05c6407db13228e
Jason Johnston <jasonjoh@users.noreply.github.com>
2018-02-21 11:46:49 -0500

    Removed X-AnchorMailbox from requests
    Graph ignores this header, so setting it is useless.

--- tutorial/views.py
+++ tutorial/views.py
@@ -33,39 +33,35 @@ def gettoken(request):
   request.session['access_token'] = access_token
   request.session['refresh_token'] = refresh_token
   request.session['token_expires'] = expiration
-  request.session['user_email'] = user['mail']
 
   return HttpResponseRedirect(reverse('tutorial:mail'))
   
 def mail(request):
   access_token = get_access_token(request, request.build_absolute_uri(reverse('tutorial:gettoken')))
-  user_email = request.session['user_email']
   # If there is no token in the session, redirect to home
   if not access_token:
     return HttpResponseRedirect(reverse('tutorial:home'))
   else:
-    messages = get_my_messages(access_token, user_email)
+    messages = get_my_messages(access_token)
     context = { 'messages': messages['value'] }
     return render(request, 'tutorial/mail.html', context)
     
 def events(request):
   access_token = get_access_token(request, request.build_absolute_uri(reverse('tutorial:gettoken')))
-  user_email = request.session['user_email']
   # If there is no token in the session, redirect to home
   if not access_token:
     return HttpResponseRedirect(reverse('tutorial:home'))
   else:
-    events = get_my_events(access_token, user_email)
+    events = get_my_events(access_token)
     context = { 'events': events['value'] }
     return render(request, 'tutorial/events.html', context)
     
 def contacts(request):
   access_token = get_access_token(request, request.build_absolute_uri(reverse('tutorial:gettoken')))
-  user_email = request.session['user_email']
   # If there is no token in the session, redirect to home
   if not access_token:
     return HttpResponseRedirect(reverse('tutorial:home'))
   else:
-    contacts = get_my_contacts(access_token, user_email)
+    contacts = get_my_contacts(access_token)
     context = { 'contacts': contacts['value'] }
     return render(request, 'tutorial/contacts.html', context)
